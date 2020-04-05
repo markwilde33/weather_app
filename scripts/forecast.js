33a707b6bf747afd5b6e60efc115ed8b6409ad40
WildeMedia <wildemedia33@gmail.com>
2020-04-05 16:49:29 +0100

    Wrote the logic to get the selected cities current weather conditions.

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,6 +1,21 @@
 //get theAPI key needed for the app
 const key = 'TbzciWL68XHGkmJfJ1bmICvLzpekjAwa'
-//create an async function to get the data
+//create an async function to get the city data, take in the current city Key as an argument for the id parameter
+const getWeather = async (id) => {
+  //base url of the api endpoint the request is being made to
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+  // query the api key by the id entered as an argument
+  const query = `${id}?apikey=${key}`;
+  //combine requests so that they are resolved together
+  const response = await fetch(base + query);
+  //get the data
+  const data = await response.json();
+  //return the data
+  return data[0];
+};
+
+
+//create an async function to get the city data
 const getCity = async (city) => {
 //base url of the api endpoint the request is being made to
 const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -13,7 +28,14 @@ const data = await response.json();
 //return the data, 1st result only
 return data[0];
 };
-//get the data returned by the promise
+//get the city returned by the promise
 getCity('leeds')
-.then(data => console.log(data))
+.then(data => {
+  // get the Key returned by the city
+  return getWeather(data.Key)
+})
+.then(data => {
+  //log the data
+  console.log(data);
+})
 .catch(err => console.log(err));
