dd8859b56ba42afb0a2e7502a8bfaadd1f281621
WildeMedia <wildemedia33@gmail.com>
2020-04-06 10:37:03 +0100

    Added an async function takes stores and returns the data returned by the getCity and getWeather async functions in forecast.js. Called this function inside the cityForm function and then logged the results to the console.

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,18 @@
 //get the form users use to search for a city
 const cityForm = document.querySelector('form');
+//this async function takes in the data returned by the getCity and getWeather async functions in forecast.js
+const updateCity = async (city) => {
+  //get the value to be returned and store it in the cityDets variable
+  const cityDets = await getCity(city);
+  //get the value to be returned and store it in the weather variable, get the Key of the inputted city stored in cityDets
+  const weather = await getWeather(cityDets.Key);
+  // return the values
+  return {
+    cityDets: cityDets,
+    weather: weather
+  };
+
+}
 
 // run the code block when a user submits the form
 cityForm.addEventListener('submit', e => {
@@ -10,4 +23,8 @@ cityForm.addEventListener('submit', e => {
   const city = cityForm.city.value.trim();
   // clear the form field after it has been submitted
   cityForm.reset();
-})
+  //update ui with the city inputted by the user
+  updateCity(city)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+});
