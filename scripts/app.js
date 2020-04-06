6a36191ff91dedffceb7936d5aca54db62ae3d35
WildeMedia <wildemedia33@gmail.com>
2020-04-06 15:07:46 +0100

    Gave an example of when to use the ternary operator.

--- scripts/app.js
+++ scripts/app.js
@@ -34,12 +34,16 @@ const updateUI = (data) => {
   icon.setAttribute('src', iconSrc);
 
   //update the night/day icon images
-  let timeSrc = null;
-  if(weather.IsDayTime) {
-    timeSrc = 'img/day.svg';
-  } else {
-    timeSrc = 'img/night.svg';
-  }
+  // let timeSrc = null;
+  // if(weather.IsDayTime) {
+  //   timeSrc = 'img/day.svg';
+  // } else {
+  //   timeSrc = 'img/night.svg';
+  // }
+
+  //use the ternary operator instead of the above conditional statement
+  let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+
   time.setAttribute('src', timeSrc);
 
   // remove the d-none class if present
